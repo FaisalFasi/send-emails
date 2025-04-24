b8595e4083c1868d74ac3eed2f00e8cc31bc1902
faisalrehman <faisalfasi18@gmail.com>
2025-04-24 17:02:57 +0200

    feat: update Firebase configuration to use environment variables

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,12 +8,12 @@ import { FirebaseStorage, getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAx37JtwJN0oAaY25e_OMt9c18vuEFFbVo",
-  authDomain: "syrena-admin-email.firebaseapp.com",
-  projectId: "syrena-admin-email",
-  storageBucket: "syrena-admin-email.firebasestorage.app",
-  messagingSenderId: "389480480168",
-  appId: "1:389480480168:web:2cee80052ccbe92451319d",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
