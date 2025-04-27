d3e9e90c134019c5a3a50a91c9d6d0a8d46b7f33
faisalrehman <faisalfasi18@gmail.com>
2025-04-27 17:45:29 +0200

    feat: update Firebase configuration and adjust Next.js settings for improved hosting compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export", // Changed from 'standalone' to 'export' for Firebase Hosting
-  distDir: "out",
+  output: "standalone", // Changed from 'standalone' to 'export' for Firebase Hosting
+  // distDir: "out",
   images: {
     unoptimized: true, // Required for static export
   },
-  trailingSlash: true, // Critical for Firebase Hosting
+  experimental: {
+    serverActions: {
+      // Either specify allowedOrigins or bodySizeLimit, or both
+      bodySizeLimit: "2mb", // Optional size limit
+    },
+  },
 };
 
 export default nextConfig;
